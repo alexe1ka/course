d07662baa4ba53a83d747f591c094bff7784e1a0
alexe1ka <leshka_09@bk.ru>
2017-10-18 20:21:44 +0300

    add testTasks

---
+++ homework3/src/com/company/TestTasks.java
@@ -0,0 +1,16 @@
+package com.company;
+
+
+public class TestTasks {
+
+    public static void main(String[] args) {
+        Task1 task1 = new Task1();
+        task1.makeWork();
+
+        Task2 task2 = new Task2();
+        task2.makeWork();
+
+//        Task4 task4 = new Task4();
+//        task4.makeWork();
+    }
+}
